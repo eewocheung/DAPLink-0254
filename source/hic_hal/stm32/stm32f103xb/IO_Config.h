b3fdffb80e66fbbd434ef402d9c872d5fcb74daa
derrick <536567043@qq.com>
2019-06-29 22:33:41 +0800

    修改stm32 usb控开关反转，CDC LED 控制脚

--- source/hic_hal/stm32/stm32f103xb/IO_Config.h
+++ source/hic_hal/stm32/stm32f103xb/IO_Config.h
@@ -33,8 +33,8 @@ COMPILER_ASSERT(DAPLINK_HIC_ID == DAPLINK_HIC_ID_STM32F103XB);
 #define USB_CONNECT_PORT_DISABLE()   __HAL_RCC_GPIOA_CLK_DISABLE()
 #define USB_CONNECT_PORT             GPIOA
 #define USB_CONNECT_PIN              GPIO_PIN_15
-#define USB_CONNECT_ON()             (USB_CONNECT_PORT->BSRR = USB_CONNECT_PIN)
-#define USB_CONNECT_OFF()            (USB_CONNECT_PORT->BRR  = USB_CONNECT_PIN)
+#define USB_CONNECT_OFF()             (USB_CONNECT_PORT->BSRR = USB_CONNECT_PIN)
+#define USB_CONNECT_ON()            (USB_CONNECT_PORT->BRR  = USB_CONNECT_PIN)
 
 //Connected LED
 #define CONNECTED_LED_PORT           GPIOB
@@ -74,9 +74,9 @@ COMPILER_ASSERT(DAPLINK_HIC_ID == DAPLINK_HIC_ID_STM32F103XB);
 #define PIN_HID_LED                  GPIO_PIN_9
 #define PIN_HID_LED_Bit              9
 
-#define PIN_CDC_LED_PORT             GPIOA
-#define PIN_CDC_LED                  GPIO_PIN_9
-#define PIN_CDC_LED_Bit              9
+#define PIN_CDC_LED_PORT             GPIOB
+#define PIN_CDC_LED                  GPIO_PIN_5
+#define PIN_CDC_LED_Bit              5
 
 #define PIN_MSC_LED_PORT             GPIOA
 #define PIN_MSC_LED                  GPIO_PIN_9
